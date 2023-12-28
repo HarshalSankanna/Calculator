ac8c2e8fcea0fe18bb04af93ebe9593c92fde5a6
Harshal <harshal0921@gmail.com>
2023-12-28 14:52:08 +0530

    created operations functions

---
+++ script.js
@@ -0,0 +1,43 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(a, b, operator) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+
+    case "-":
+      return subtract(a, b);
+
+    case "*":
+      return multiply(a, b);
+
+    case "/":
+      return divide(a, b);
+
+    default:
+      break;
+  }
+}
+
+// console.log(operate(5, 2, "-"));
+
+let a;
+let b;
+let operator;
+
+let display = document.querySelector(".display");
+display.addEventListener("", () => {});
